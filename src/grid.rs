d027c91b9673cd61598ff6b77b6460fa61b04a8e
Saskapult <thekieransmith@gmail.com>
2024-03-24 22:59:34 -0700

    smoothing basic

--- src/grid.rs
+++ src/grid.rs
@@ -16,8 +16,9 @@ pub struct Cage {
 pub enum GridCell {
     Uninitialized,
     Exterior,
-	Boundary(f32),
-    Interior(f32),
+	// Holds index into data, so will be multiple of cage len
+	Boundary(usize),
+    Interior(usize),
 	// InteriorControl(f32), // Would need to be part of the Cage data structure, maybe rename to ControlThingy
 }
 impl GridCell {
@@ -27,6 +28,25 @@ impl GridCell {
 			_ => false,
 		}
 	}
+
+	pub fn mark_boundary(&mut self, data: &mut Vec<f32>, cage_len: usize) {
+		match self {
+			Self::Uninitialized => {
+				let i = data.len();
+				*self = GridCell::Boundary(i);
+				data.extend((0..cage_len).map(|_| 0.0));
+			},
+			Self::Boundary(i) => {},
+			_ => panic!(),
+		}
+	}
+
+	pub fn get_boundary<'a>(&self, data: &'a mut Vec<f32>, cage_len: usize) -> &'a mut [f32] {
+		match self {
+			Self::Boundary(i) => &mut data[*i..*i+cage_len],
+			_ => panic!(),
+		}
+	}
 }
 
 
@@ -34,7 +54,8 @@ pub struct Grid {
     pub min: Vec3,
     pub max: Vec3,
     pub dimensions: UVec3, 
-    data: Vec<GridCell>, // [[Gridcell; number of cage points]; size of volume cubed]
+    cell_types: Vec<GridCell>, // [Gridcell size of volume cubed]
+	cell_data: Vec<f32>, // Vec<[f32; number of cage points]>
 	pub cage: Cage
 }
 impl Grid {
@@ -46,12 +67,12 @@ impl Grid {
 			.reduce(|a, v| a.max(v))
 			.unwrap();
 		
-		let data_size = (dimensions[0] * dimensions[1] * dimensions[2]) as usize * cage.vertices.len();
-		let mut data = Vec::with_capacity(data_size);
-		data.resize(data_size, GridCell::Uninitialized);
+		let data_size = (dimensions[0] * dimensions[1] * dimensions[2]) as usize;
+		let mut cell_types = Vec::with_capacity(data_size);
+		cell_types.resize(data_size, GridCell::Uninitialized);
 
 		Self {
-			min, max, dimensions, data, cage, 
+			min, max, dimensions, cell_types, cell_data: Vec::new(), cage, 
 		}
 	}
 
@@ -66,17 +87,6 @@ impl Grid {
 			(position[0] * self.dimensions[0] * self.dimensions[1] * 
 			position[1] * self.dimensions[1] * 
 			position[3]) as usize)
-	}
-
-	#[inline]
-	fn get_data(&self, index: usize) -> &[GridCell] {
-		&self.data[index..index+self.depth()]
-	}
-
-	#[inline]
-	fn get_data_mut(&mut self, index: usize) -> &mut [GridCell] {
-		let r = index..index+self.depth();
-		&mut self.data[r]
 	}
 
 	// This is probably the least efficent part of the implementation 
@@ -110,11 +120,13 @@ impl Grid {
 						let barycentric = barycentric(centre, v0, v1, v2);
 
 						let cell_index = self.index_of(cell).unwrap();
-						let cell_data = self.get_data_mut(cell_index);
+						self.cell_types[cell_index].mark_boundary(&mut self.cell_data, self.cage.vertices.len());
 
-						cell_data[i0] = GridCell::Boundary(barycentric[0]);
-						cell_data[i1] = GridCell::Boundary(barycentric[1]);
-						cell_data[i2] = GridCell::Boundary(barycentric[2]);
+						let cell_data = self.cell_types[cell_index].get_boundary(&mut self.cell_data, self.cage.vertices.len());
+
+						cell_data[i0] = barycentric[0];
+						cell_data[i1] = barycentric[1];
+						cell_data[i2] = barycentric[2];
 					}
 				}
 			}
@@ -128,11 +140,9 @@ impl Grid {
 		while let Some(pos) = stack.pop() {
 			let index = self.index_of(pos.as_uvec3()).unwrap();
 			// If uninitialized
-			if self.get_data(index).iter().all(|cell| cell.uninitialized()) {
+			if self.cell_types[index].uninitialized() {
 				// Mark as exerior 
-				for cell in self.get_data_mut(index) {
-					*cell = GridCell::Exterior;
-				}
+				self.cell_types[index] = GridCell::Exterior;
 
 				// Add in-bounds neighbours to stack 
 				for d in [IVec3::X, IVec3::Y, IVec3::Z, -IVec3::X, -IVec3::Y, -IVec3::Z] {
@@ -147,9 +157,13 @@ impl Grid {
 	}
 
 	pub fn mark_interior(&mut self) {
-		for cell in self.data.iter_mut() {
+		for cell in self.cell_types.iter_mut() {
 			match cell {
-				GridCell::Uninitialized => *cell = GridCell::Interior(0.0),
+				GridCell::Uninitialized => {
+					let i = self.cell_data.len();
+					self.cell_data.extend((0..self.cage.vertices.len()).map(|_| 0.0));
+					*cell = GridCell::Interior(i);
+				},
 				_ => {},
 			}
 		}
@@ -158,12 +172,84 @@ impl Grid {
 	// Most basic cpu smoothing
 	// Should be terrible, but good for presentation
 	pub fn smooth_basic(&mut self, tau: f32) {
-		
+		let mut buffer = Vec::with_capacity(self.cell_data.len());
+		let [sx, sy, sz] = self.dimensions.as_ivec3().to_array();
+		let positions = (0..sx).flat_map(move |x| {
+			(0..sy).flat_map(move |y| {
+				(0..sz).map(move |z| {
+					IVec3::new(x, y, z)
+				})
+			})
+		})
+		// Idea 1: filter by cell
+		// Test performance with and without this
+		.filter(|&cell| {
+			let index = self.index_of(cell.as_uvec3()).unwrap();
+			if let GridCell::Interior(_) = self.cell_types[index] {
+				true
+			} else {
+				false
+			}
+		})
+		.collect::<Vec<_>>();
 
+		loop {
+			// Store previous state
+			buffer.clone_from(&self.cell_data);
+
+			let delta_accum = 0.0;
+			let delta_count = 0;
+
+			// Idea 2: chunks for rayon jobs
+			// Map to (mutable reference (unsafe), position)
+			for cell in positions.iter().copied() {
+				let index = self.index_of(cell.as_uvec3()).unwrap();
+				
+				// Only if interior
+				if let GridCell::Interior(_) = self.cell_types[index] {
+				} else {
+					continue 
+				}
+
+				let cell_data = self.cell_types[index].get_boundary(&mut self.cell_data, self.cage.vertices.len());
+				// Zero value for sum
+				cell_data.iter_mut().for_each(|d| *d = 0.0);
+
+				// For each neighbour
+				let mut neighbour_count = 0;
+				for d in [
+					IVec3::X,
+					IVec3::Y,
+					IVec3::Z,
+					IVec3::NEG_X,
+					IVec3::NEG_Y,
+					IVec3::NEG_Z,
+				] {
+					let n = cell + d;
+					// If in bounds
+					if n.cmpge(IVec3::ZERO).all() && n.cmplt(self.dimensions.as_ivec3()).all() {
+						let d_index = self.index_of(n.as_uvec3()).unwrap();
+						let n_data = self.cell_types[d_index].get_boundary(&mut buffer, self.cage.vertices.len());
+
+						// Add to sum
+						cell_data.iter_mut().zip(n_data).for_each(|(c, n)| *c += *n);
+						neighbour_count += 1;
+					}
+				}
+
+				// Cell is average of neighbours
+				cell_data.iter_mut().for_each(|d| *d /= neighbour_count as f32);				
+			}
+
+			let delta = delta_accum / delta_count as f32;
+			if delta <= tau {
+				break
+			}
+		}
 
 	}
 
-	// Raton job for each entry in data 
+	// Rayon jobs! 
 	pub fn smooth_threads(&mut self, tau: f32) {
 
 	}
@@ -197,10 +283,8 @@ fn aabb_triangle_intersect(
 	let u1 = Vec3::Y;
 	let u2 = Vec3::Z;
 
-	
 	for axis in [
-		// 9 axis 
-		u0.cross(f0),
+		u0.cross(f0), // 9 axis 
 		u0.cross(f1),
 		u0.cross(f2),
 		u1.cross(f0),
@@ -209,12 +293,10 @@ fn aabb_triangle_intersect(
 		u2.cross(f0),
 		u2.cross(f1),
 		u2.cross(f2),
-		// 3 face normals for AABB
-		Vec3::X,
+		Vec3::X, // 3 face normals for AABB
 		Vec3::Y,
 		Vec3::Z,
-		// 1 face normal for triangle 
-		f0.cross(f1),
+		f0.cross(f1), // 1 face normal for triangle 
 	] {
 		let p0 = v0.dot(axis);
 		let p1 = v1.dot(axis);
